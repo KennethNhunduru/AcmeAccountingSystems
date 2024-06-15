8cb3b88c7daf20fad159b04b45055d94536e9edd
KennethNhunduru <133786329+KennethNhunduru@users.noreply.github.com>
2024-06-15 04:20:27 +0100

    Add project files.

---
+++ AcmeAccountingSystems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AcmeAccountingSystems
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            //creating the first string
+            string str1 = "Welcome to Acme Accounting Systems";
+            Console.WriteLine(str1);
+
+            //creating the second string
+            string str2 = "Remember, we're \"accounting\" on you!";
+            Console.WriteLine(str2);
+            
+            Console.ReadLine();
+        }
+    }
+}
